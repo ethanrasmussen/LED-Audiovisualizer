2d31b2b41c01d0fb6d65cfe476bb729b75d5cfec
ethanrasmussen <itseraz@gmail.com>
2020-12-27 10:30:14 -0600

    set up rpi gpio/led control

--- pie/main.py
+++ pie/main.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import *
 from flask import request
+from pie.gpio_control import set_leds
 
+
+# set list of GPIO pin's that make up LED audiovisualizer
+pinlist = []
 
 # init Flask app
 app = Flask(__name__)
@@ -17,7 +21,11 @@ def leds():
     # process arg to get num led's to enable
     num_leds = request.args.get('num_leds')
     print(num_leds)
+    # if number of led's to set is higher than possible, set to max
+    if num_leds > len(pinlist):
+        num_leds = len(pinlist)
     # trigger LEDs
+    set_leds(num_leds, pinlist)
     return num_leds
 
 # run app
