35b6b3e20fbc4f137a4acd70a92393f834703e82
ethanrasmussen <itseraz@gmail.com>
2020-12-27 10:27:14 -0600

    move gpio control

--- pie/main.py
+++ pie/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import *
+from flask import request
 
 
 # init Flask app
@@ -11,6 +12,13 @@ def index():
     return render_template('index.html')
 
 # led trigger route
+@app.route('/leds', methods=['GET'])
+def leds():
+    # process arg to get num led's to enable
+    num_leds = request.args.get('num_leds')
+    print(num_leds)
+    # trigger LEDs
+    return num_leds
 
 # run app
 if __name__ == '__main__':
