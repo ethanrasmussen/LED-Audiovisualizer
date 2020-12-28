053f4ac0ef96b35b675c415ad35404c6bee35bbc
ethanrasmussen <itseraz@gmail.com>
2020-12-28 12:24:32 -0600

    adjust sleep to account for processing time between 2 APIs (better audio/visual sync)

--- website/visualizer_control.py
+++ website/visualizer_control.py
@@ -61,7 +61,7 @@ def play_visualizer(audiofile_path:str, led_values, rpi_ip:str):
     for val in led_values:
         r = requests.get(url=f"http://{rpi_ip}/leds?num_leds={val}")
         print(val)
-        time.sleep(0.1)
+        time.sleep(0.04)
     # reset LED's to 0
     r = requests.get(url=f"http://{rpi_ip}/leds?num_leds=0")
     pygame.mixer.music.stop()
